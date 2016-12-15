e01fb867b2ed0a406abc38f785082e5de122b059
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2016-12-15 13:27:24 +0900

    add fixture of kindle

--- spec/factories/kindles.rb
+++ spec/factories/kindles.rb
@@ -1,10 +1,11 @@
+# coding: utf-8
 FactoryGirl.define do
   factory :kindle do
-    asin "MyString"
-    title "MyString"
-    publisher "MyString"
-    authors ""
-    image_url "MyString"
-    publish_at "2016-12-14"
+    asin "B01N3PNATY"
+    title "バトルスタディーズ（８） (モーニングコミックス)"
+    publisher "講談社"
+    authors ["メーブ", "恵広史"]
+    image_url "https://images-fe.ssl-images-amazon.com/images/I/51ZcS0q0-BL.jpg"
+    publish_at "2016-12-01"
   end
 end
